b63b90d1d8faf1300118321dda420537afe875e8
Bernardo Rodrigues <bernrod@gmail.com>
2019-07-07 01:56:01 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.31557be18a417a0a6dc638f1b342eac4.js"
 );
 
 self.addEventListener('message', (event) => {
